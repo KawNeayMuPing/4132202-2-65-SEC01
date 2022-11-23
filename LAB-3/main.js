533b25e6fef132bb7005d0bd7e731ac3e6d8f71a
kittipat1 <640112418087@bru.ac.th>
2022-11-23 11:43:37 +0700

    LAB-3

---
+++ LAB-3/main.js
@@ -0,0 +1,58 @@
+
+    var num = 10;
+    let name = "big";
+    // let Array = [10,15,"big"];
+    const obj = {name: "big", age: 19 };
+    const objMix = {
+        data:[
+            {name: "Sword", gpa: 4.3 },
+            {name: "Red", gpa: 2.8 }
+        ]
+    }
+    
+    console.log(num);
+    console.log(Array);
+    console.log(objMix);
+
+    let string = num + name;
+    console.log(string);
+
+    string = objMix.data[1].name + Array[0];
+    console.log(string);
+
+    string = "<p>" + Array[2] + "</p>";
+    string = `<p>${Array}</p>`;
+
+    console.log(string);
+
+    function Add(a, b) {
+        return a+b;
+    }
+    console.log(Add(3, 5));
+
+    document.getElementById("div1").innerHTML = name;
+
+    $(function () {
+        // alert("Hello");
+        $("#div1").html("Tanachod");
+
+        $("#bt1").bind("click", function () {
+            $("#div1").html("I Love IT"); 
+
+        // $("#bt1").Click(function () {
+           //  $("#div1").html("I Love IT");
+        });
+
+        $("#bt3").click(() => {
+            // arrow function
+        $(".in1").val("BRU");
+        });
+
+        $("#bt4").click(() => {
+            $(".in1:odd").addClass("red");
+        });
+
+        $("#div_about").load("./pages/about.html");
+
+        
+    }); //jQuery Handle
